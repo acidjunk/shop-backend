b71249f1854a74bdabb22a8648f0ebfacce231bc
hridho <500860@student.fontys.nl>
2025-03-27 13:06:58 +0100

    Unit test added for early_access

---
+++ tests/unit_tests/api/test_early_access.py
@@ -0,0 +1,31 @@
+from server.db.models import EarlyAccessTable
+from server.utils.json import json_dumps
+
+
+def test_early_access_create(test_client):
+    body = {"email": "support@shopVirge.com"}
+
+    response = test_client.post(f"/early-access", data=json_dumps(body))
+    assert response.status_code == 201
+    early_access = EarlyAccessTable.query.first()
+    assert early_access.email == "support@shopvirge.com"
+
+
+def test_early_access_create_wrong_email(test_client):
+    body = {"email": "supportshopVirge.com"}
+
+    response = test_client.post(f"/early-access", data=json_dumps(body))
+    assert response.status_code == 422
+    assert "value is not a valid email" in response.json()["detail"][0]["msg"]
+
+
+# def test_early_access_create_duplicate(test_client):
+#     body = {
+#         "email": "supportshopVirge.com"
+#     }
+#
+#     response = test_client.post(f"/early-access", data=json_dumps(body))
+#     assert response.status_code == 422
+#     assert "value is not a valid email" in response.json()["detail"][0]["msg"]
+#     # early_access = earlyAccessTable.query.filter_by(id=response.json()["id"]).first()
+#     # assert early_access.email == "support@shopVirge.com"
