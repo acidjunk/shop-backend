bcae9a9af05f24987d742bf5c4c9c20315df210d
ViggoSeerden <491216@student.fontys.nl>
2025-04-15 17:36:40 +0200

    ported wfo mailing setup

--- server/api/endpoints/shop_endpoints/info_request.py
+++ server/api/endpoints/shop_endpoints/info_request.py
@@ -8,8 +8,14 @@ from fastapi.param_functions import Body
 from pydantic import ValidationError
 from sqlalchemy.exc import IntegrityError
 
+from server.api.helpers import load
 from server.crud.crud_info_request import info_request_crud
+from server.crud.crud_product import product_crud
+from server.db.models import ShopTable
+from server.mail import generate_confirmation_mail, send_mail
 from server.schemas.info_request import InfoRequestCreate
+from server.settings import mail_settings
+from server.utils.types import MailAddress, MailType
 
 logger = structlog.get_logger(__name__)
 
@@ -20,8 +26,26 @@ router = APIRouter()
 def create_info_request(data: InfoRequestCreate = Body(...)) -> Any:
     try:
         logger.info("Saving early access", data=data)
-        info_request = info_request_crud.create(obj_in=data)
-        return info_request
+        info_request_crud.create(obj_in=data)
+
+        product = product_crud.get_id_by_shop_id(data.shop_id, data.product_id)
+        shop = load(ShopTable, data.shop_id)
+
+        if product is None or shop is None:
+            raise HTTPException(
+                status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+                detail="The product or shop does not exist.",
+            )
+
+        contact_persons = [MailAddress(email=data.email, name="")]
+
+        confirmation_mail = generate_confirmation_mail(product, MailType.INFO, shop.name, contact_persons, None, None)
+        if mail_settings.SHOP_MAIL_ENABLED:
+            logger.info("Sending info mail", email=data.email)
+            send_mail(confirmation_mail)
+        else:
+            logger.info("Sending the email is disabled for this specific step, not actually sending it.")
+        return {"confirmation_mail": confirmation_mail}
 
     except ValidationError as ve:
         # Log the validation error
